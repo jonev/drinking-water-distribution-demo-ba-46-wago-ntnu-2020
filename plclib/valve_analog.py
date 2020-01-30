9cabc971610fd8ef0195fe03e4d00d22b3bbed32
Peder Ward <pederw455@gmail.com>
2020-01-30 12:27:58 +0100

    Init commit

--- plclib/valve_analog.py
+++ plclib/valve_analog.py
@@ -3,7 +3,17 @@ class ValveAnalog:
     Features:
     - Auto/manual
     - Transfer alarm
+    In:
+    Manual control value form HMI
+    Auto control value from logic
+    Manual and auto command from HMI
+    Out:
+    ControlValue
     """
+
+    def controlLogic(self):  # Bør klare oss uten en slik
+        # Check for alarm etc
+        pass
 
     def __init__(self, tag):
         self.__tag = tag  # TODO
