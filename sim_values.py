092b374cee63df440a7a330055d363cb5d735a78
Peder Ward <pederw455@gmail.com>
2020-02-18 19:03:17 +0100

    randomWeather changes

--- sim_values.py
+++ sim_values.py
@@ -22,20 +22,20 @@ class WeatherForcast:
         self.level = 50
         self.valve = 0
         self.counter = 0
-        self.random = 11  # Startvalue
         self.checklist = [0, 0, 0, 0, 0, 0]
         self.sumrain = 0
         self.weatherTypes = (
-            ["sun"] * 3
-            + ["partlycloudy"] * 3
-            + ["cloudy"] * 5
-            + ["drizzle"] * 5
-            + ["rainy"] * 5
-            + ["storm"] * 3
+            ["sun"]  # * 3
+            + ["partlycloudy"]  # * 3
+            + ["cloudy"]  # * 5
+            + ["drizzle"]  # * 5
+            + ["rainy"]  # * 5
+            + ["storm"]  # * 3
         )
+        self.random = int(len(self.weatherTypes) / 2)  # Startvalue
 
     def randomWeather(self,):
-        self.random = self.random + randint(-4, 5)  # Logic for at været ikke kan endres plutselig
+        self.random = self.random + randint(-1, 1)  # Logic for at været ikke kan endres plutselig
         if self.random >= len(self.weatherTypes):
             self.random = len(self.weatherTypes) - 1
         elif self.random < 0:
@@ -61,6 +61,23 @@ class WeatherForcast:
 
 
 """
+    def check(self):  # For og sjekke gjennomsnittlig vær over tid
+        if self.weather == "sun":
+            self.checklist[0] = self.checklist[0] + 1
+        elif self.weather == "partlycloudy":
+            self.checklist[1] = self.checklist[1] + 1
+        elif self.weather == "cloudy":
+            self.checklist[2] = self.checklist[2] + 1
+        elif self.weather == "drizzle":
+            self.checklist[3] = self.checklist[3] + 1
+        elif self.weather == "rainy":
+            self.checklist[4] = self.checklist[4] + 1
+        elif self.weather == "storm":
+            self.checklist[5] = self.checklist[5] + 1
+        print(self.checklist)
+
+
+
     def waterLevel(self):
         if self.valve == 1:
             self.level = self.level - 5
@@ -78,37 +95,24 @@ class WeatherForcast:
             self.valve = 0
             print("Valve closed")
 
-    def check(self):  # For og sjekke gjennomsnittlig vær over tid
-
-        if self.weather == "sun":
-            self.checklist[0] = self.checklist[0] + 1
-        elif self.weather == "partlycloudy":
-            self.checklist[1] = self.checklist[1] + 1
-        elif self.weather == "cloudy":
-            self.checklist[2] = self.checklist[2] + 1
-        elif self.weather == "drizzle":
-            self.checklist[3] = self.checklist[3] + 1
-        elif self.weather == "rainy":
-            self.checklist[4] = self.checklist[4] + 1
-        elif self.weather == "storm":
-            self.checklist[5] = self.checklist[5] + 1
-        print(self.checklist)
 """
+
 
 weatherForcast = WeatherForcast()
 while True:
 
     # for x in range(0, 876):  # På yr er ny varsling vær time. Mulig vi må ha det kontunuerlig
-    # mqtt.publish("wago/ba/sim/waterlevel", "6")
+
     randomWeather = weatherForcast.randomWeather()
     rain = weatherForcast.rainWeather()
-    # print(randomWeather)
+
+    # weatherForcast.randomWeather()
     # weatherForcast.check()
     # weatherForcast.rainWeather()
     # weatherForcast.openValve()
     # weatherForcast.waterLevel()
     mqtt.publish("wago/ba/sim/out/randomWeather", randomWeather.__str__())
     mqtt.publish("wago/ba/sim/out/rain", rain.__str__())
-    # mqtt.publish("wago/ba/sim/waterlevel", "6")
-    time.sleep(3)
+
+    time.sleep(1)
 
