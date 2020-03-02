bf9407467bbf0cb6fea0572434be4e7353df38ba
jonev <jone.vassbo@gmail.com>
2020-03-02 14:39:13 +0100

    Dynamic mapping from opc ua to mqtt and visa versa done

--- mqtt_opc_ua_link.py
+++ mqtt_opc_ua_link.py
@@ -1,95 +1,150 @@
 from opcua import Client, ua
 import paho.mqtt.client as mqtt
-from threading import Thread
+from threading import Thread, Lock
 import time
 import json
 import hashlib
 
+# Global variables
+variables = {}
+hashs = {}  # Storing a hash of the object to be able to compare two objects fast
+hashsLock = Lock()  # hashs are used in multiple threads
+sampleTime = 30  # For testing
+## Opc UA
+opcUaServer = "172.17.0.1"  # Host of docker
+opcUaServerUsername = "admin"
+opcUaServerPassword = "wago"
+opcUaNs = 4  # Address
+opcUaIdPrefix = "|var|WAGO 750-8214 PFC200 G2 2ETH RS CAN.PFCx00_SmartCoupler.HMI"
+## MQTT
+mqttBroker = "broker.hivemq.com"
+mqttPort = 1883
+mqttTopicPublishData = "ba/wago/opcua/test/plcpub"
+mqttTopicSubscribeData = "ba/wago/opcua/test/plcsub"
+mqttPublishSuffix = "Pv"  # Published every sample, other tags are pulished on data change
+
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-    client.subscribe("ba/wago/data/test/motta/#")
+    print("MQTT Connected with result code " + str(rc))
+    client.subscribe(mqttTopicSubscribeData)
 
 
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    global hashsLock
+    try:
+        receivedObject = json.loads(str(msg.payload, encoding="utf-8"))
+        # Generate new hash
+        newValueHash = hashlib.md5(receivedObject.__str__().encode("utf-8")).hexdigest()
+        # Store hash
+        with hashsLock:  # Sending data only on change, therefor no need to check for change
+            hashs[receivedObject["_id"]] = newValueHash
+        # Write to opc ua by getting the children recursive
+        topLevelObject = clientPlc.get_node(
+            "ns=" + str(opcUaNs) + ";s=" + opcUaIdPrefix + "." + receivedObject["_id"]
+        )
+        setChildrenRecursive(receivedObject, topLevelObject.get_children())
+    except Exception as e:
+        print(e)
 
 
-clientPlc = Client("opc.tcp://192.168.0.15:4840")
-clientPlc.set_user("admin")
-clientPlc.set_password("wago")
-ns = 4
-HmiId = "|var|WAGO 750-8214 PFC200 G2 2ETH RS CAN.PFCx00_SmartCoupler.HMI"
-variables = {}
-nodes = {}
-hashs = {}
-
-
-def getChildrenRecursive(jsonObject, OpcNodes):
-    # children = OpcNode.get_children()
+def setChildrenRecursive(pObject, OpcNodes):
     for node in OpcNodes:
         children = node.get_children()
+        tagname = getTagname(node)
         if len(children) == 0:
-            path = node.nodeid.Identifier
-            position = path.rfind(".")
-            tagname = path[position + 1 :]
-            jsonObject[tagname] = node.get_value()
+            value = pObject[tagname]
+            if type(value) is str:
+                node.set_value(value)
+            elif type(value) is bool:
+                node.set_value(value)
+            elif type(value) is float:
+                node.set_value(value, varianttype=ua.VariantType.Float)
+            else:
+                raise Exception("Type not found")
         else:
-            path = node.nodeid.Identifier
-            position = path.rfind(".")
-            tagname = path[position + 1 :]
-            jsonObject[tagname] = {}
-            getChildrenRecursive(jsonObject[tagname], children)
+            setChildrenRecursive(pObject[tagname], children)
+
+
+# OPC UA
+clientPlc = Client("opc.tcp://" + opcUaServer + ":4840")
+clientPlc.set_user(opcUaServerUsername)
+clientPlc.set_password(opcUaServerPassword)
+
+# MQTT
+mqttClient = mqtt.Client()
+mqttClient.on_connect = on_connect
+mqttClient.on_message = on_message
+
+
+def getChildrenRecursive(pObject, OpcNodes):
+    for node in OpcNodes:
+        children = node.get_children()
+        tagname = getTagname(node)
+        if len(children) == 0:
+            pObject[tagname] = node.get_value()
+        else:
+            pObject[tagname] = {}
+            getChildrenRecursive(pObject[tagname], children)
+
+
+def getTagname(node):
+    path = node.nodeid.Identifier
+    position = path.rfind(".")
+    return path[position + 1 :]
 
 
 try:
     # Init
+    print("Connecting to Opc.")
     clientPlc.connect()
-    HMITags = clientPlc.get_node("ns=" + str(ns) + ";s=" + HmiId)
-    mqttClient = mqtt.Client()
-    mqttClient.on_connect = on_connect
-    mqttClient.on_message = on_message
-    mqttClient.connect("broker.hivemq.com", 1883, 60)
+    if clientPlc is None:
+        raise Exception("Opc connection failed")
+    print("Connected")
+    nodesUnderPrefix = clientPlc.get_node("ns=" + str(opcUaNs) + ";s=" + opcUaIdPrefix)
+
+    print("Connecting to MQTT broker.")
+    mqttClient.connect(mqttBroker, mqttPort, 60)
     mqttThread = Thread(target=mqttClient.loop_forever, args=())
     mqttThread.start()
-    time.sleep(2)  # Time to connect mqtt
+    print("Waiting for MQTT to connect...")
+    time.sleep(2)
 
-    # Loop
-    while True:
-        topLevelObjects = HMITags.get_children()
-        if len(topLevelObjects) == 0:
+    while True:  # Read data from OPC UA and Publish data to MQTT loop
+        topLevelOpcNodes = (
+            nodesUnderPrefix.get_children()
+        )  # Nodes are initialized each loop -> no need for restart if there are new nodes
+        if len(topLevelOpcNodes) == 0:
             raise Exception("No tags where found")
-        for Object in topLevelObjects:
-            path = Object.nodeid.Identifier
-            position = path.rfind(".")
-            tagname = path[position + 1 :]
-            variables[tagname] = {}
-            getChildrenRecursive(variables[tagname], Object.get_children())
+        for topLevelOpcNode in topLevelOpcNodes:
+            tagname = getTagname(topLevelOpcNode)
+            pObject = {}  # Building python object, then converting to json before sending
+            getChildrenRecursive(pObject, topLevelOpcNode.get_children())
             # Publishing
-            jsondump = json.dumps(variables[tagname])
-            if "Pv" in tagname:
-                mqttClient.publish("ba/wago/opcua/test", payload=jsondump)
+            jsonStringOfObject = json.dumps(pObject)
+            if mqttPublishSuffix in tagname:
+                mqttClient.publish(mqttTopicPublishData, payload=jsonStringOfObject)
             else:
                 # Hash
-                newValueHash = hashlib.md5(jsondump.encode("utf-8")).hexdigest()
-                if tagname in hashs:
-                    # Compare
-                    if hashs[tagname] != newValueHash:
-                        mqttClient.publish("ba/wago/opcua/test", payload=jsondump)
-                        # Publish and save hash
+                newValueHash = hashlib.md5(pObject.__str__().encode("utf-8")).hexdigest()
+                with hashsLock:
+                    if tagname in hashs:
+                        # Compare
+                        if hashs[tagname] != newValueHash:
+                            mqttClient.publish(mqttTopicPublishData, payload=jsonStringOfObject)
+                            # Publish and save hash
+                            hashs[tagname] = newValueHash
+                        # Or nothing
+                    else:
+                        # Tagname does not exist in hashs
+                        # Save hash
                         hashs[tagname] = newValueHash
-                    # Or nothing
-                else:
-                    # Tagname does not exist in hashs
-                    # Save hash
-                    hashs[tagname] = newValueHash
-                    # Publish
-                    mqttClient.publish("ba/wago/opcua/test", payload=jsondump)
-        time.sleep(10)
-
+                        # Publish
+                        mqttClient.publish(mqttTopicPublishData, payload=jsonStringOfObject)
+        time.sleep(sampleTime)
 
 except Exception as e:
     print(e)
 finally:
-    clientPlc.disconnect()
+    if clientPlc is not None:
+        clientPlc.disconnect()
     mqttClient.disconnect()
