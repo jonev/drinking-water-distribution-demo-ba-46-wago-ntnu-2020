3cf469635e38bb74a0541608e4aa22cd7bb7d53d
jonev <jone.vassbo@gmail.com>
2020-03-09 21:34:32 +0100

    Testing done for opc ua mqtt link

--- OPCUA-MQTT-link/HMI/mqtt_opc_ua_link.py
+++ OPCUA-MQTT-link/HMI/mqtt_opc_ua_link.py
@@ -7,15 +7,20 @@ import datetime
 import json
 import hashlib
 import os
+import logging
+
+logging.basicConfig(level=logging.WARNING)
 
 load_dotenv()
+
 # Global variables
 variables = {}
 hashs = {}  # Storing a hash of the object to be able to compare two objects fast
 hashsLock = Lock()  # hashs are used in multiple threads
 sampleTime = int(os.getenv("SAMPLE_TIME"))  # For testing
+PlcIsRunning = [True, True, False]
 ## Opc UA
-opcUaServer = "192.168.0.15"  # os.getenv("OPC_UA_SERVER")  # Host of docker
+opcUaServer = os.getenv("OPC_UA_SERVER")  # Host of docker
 opcUaServerUsername = os.getenv("OPC_UA_SERVER_USERNAME")
 opcUaServerPassword = os.getenv("OPC_UA_SERVER_PASSWORD")
 opcUaNs = int(os.getenv("OPC_UA_NS"))  # Address
@@ -23,12 +28,19 @@ opcUaIdPrefix = os.getenv("OPC_UA_ID_PREFIX")
 ## MQTT
 mqttBroker = os.getenv("MQTT_BROKER")
 mqttPort = int(os.getenv("MQTT_PORT"))
-mqttTopicPublishData = os.getenv("MQTT_TOPIC_PUBLISHDATA")
-mqttTopicSubscribeData = os.getenv("MQTT_TOPIC_SUBSCRIBEDATA")
+mqttTopicPublishData = [
+    "ba/wago/opcua/plc1/plcsub",
+    "ba/wago/opcua/plc2/plcsub",
+    "ba/wago/opcua/plc3/plcsub",
+]
+mqttTopicSubscribeData = [
+    "ba/wago/opcua/plc1/plcpub",
+    "ba/wago/opcua/plc2/plcpub",
+    "ba/wago/opcua/plc3/plcpub",
+]
 mqttPublishPvSuffix = os.getenv(
     "MQTT_PUBLISH_PV_SUFFIX"
 )  # Published every sample, other tags are pulished on data change
-mqttPublishPvFlag = os.getenv("MQTT_PUBLISH_PV_FLAG")
 print(
     "Evn: OpcUaServer: "
     + opcUaServer
@@ -41,25 +53,33 @@ print(
 
 def on_connect(client, userdata, flags, rc):
     print("MQTT Connected with result code " + str(rc))
-    client.subscribe(mqttTopicSubscribeData)
+    for topic in mqttTopicSubscribeData:
+        client.subscribe(topic)
 
 
 def on_message(client, userdata, msg):
-    global hashsLock
+    global hashsLock, PlcIsRunning
     try:
         receivedObject = json.loads(str(msg.payload, encoding="utf-8"))
+        if receivedObject["_owner"] == "plc1":
+            PlcIsRunning[0] = True
+        if receivedObject["_owner"] == "plc2":
+            PlcIsRunning[1] = True
+        if receivedObject["_owner"] == "plc3":
+            PlcIsRunning[2] = True
         # Generate new hash
+        del receivedObject["_timestamp"]
         newValueHash = hashlib.md5(receivedObject.__str__().encode("utf-8")).hexdigest()
         # Store hash
         with hashsLock:  # Sending data only on change, therefor no need to check for change
-            hashs[receivedObject["_id"]] = newValueHash
+            hashs[receivedObject["_tagId"]] = newValueHash
         # Write to opc ua by getting the children recursive
         topLevelObject = clientPlc.get_node(
-            "ns=" + str(opcUaNs) + ";s=" + opcUaIdPrefix + "." + receivedObject["_id"]
+            "ns=" + str(opcUaNs) + ";s=" + opcUaIdPrefix + "." + receivedObject["_tagId"]
         )
         setChildrenRecursive(receivedObject, topLevelObject.get_children())
     except Exception as e:
-        print(e)
+        logging.exception("Exception in on_message.")
 
 
 def setChildrenRecursive(pObject, OpcNodes):
@@ -74,6 +94,8 @@ def setChildrenRecursive(pObject, OpcNodes):
                 node.set_value(value)
             elif type(value) is float:
                 node.set_value(value, varianttype=ua.VariantType.Float)
+            elif type(value) is int:
+                node.set_value(value, varianttype=ua.VariantType.Int16)
             else:
                 raise Exception("Type not found")
         else:
@@ -108,6 +130,21 @@ def getTagname(node):
     return path[position + 1 :]
 
 
+def publish(pObject):
+    if pObject["_owner"] == "plc1":
+        mqttClient.publish(mqttTopicPublishData[0], payload=json.dumps(pObject))
+    elif pObject["_owner"] == "plc2":
+        mqttClient.publish(mqttTopicPublishData[1], payload=json.dumps(pObject))
+    elif pObject["_owner"] == "plc3":
+        mqttClient.publish(mqttTopicPublishData[2], payload=json.dumps(pObject))
+    elif pObject["_owner"] == "all":
+        mqttClient.publish(mqttTopicPublishData[0], payload=json.dumps(pObject))
+        mqttClient.publish(mqttTopicPublishData[1], payload=json.dumps(pObject))
+        mqttClient.publish(mqttTopicPublishData[2], payload=json.dumps(pObject))
+    else:
+        raise Exception("Unknown owner of object: " + pObject["_owner"])
+
+
 try:
     while True:
         try:
@@ -138,34 +175,30 @@ try:
                     getChildrenRecursive(pObject, topLevelOpcNode.get_children())
                     # Publishing
                     if mqttPublishPvSuffix in tagname:
-                        if mqttPublishPvFlag:
-                            pObject["_timestamp"] = str(datetime.datetime.now())
-                            mqttClient.publish(mqttTopicPublishData, payload=json.dumps(pObject))
+                        pass
                     else:
                         # Hash
                         newValueHash = hashlib.md5(pObject.__str__().encode("utf-8")).hexdigest()
                         pObject["_timestamp"] = str(datetime.datetime.now())
                         with hashsLock:
-                            if tagname in hashs:
+                            if (
+                                tagname in hashs and pObject["_type"] != ""
+                            ):  # Missing data from plc -> publish to get
                                 # Compare
                                 if hashs[tagname] != newValueHash:
-                                    mqttClient.publish(
-                                        mqttTopicPublishData, payload=json.dumps(pObject)
-                                    )
                                     # Publish and save hash
                                     hashs[tagname] = newValueHash
+                                    publish(pObject)
                                 # Or nothing
                             else:
                                 # Tagname does not exist in hashs
                                 # Save hash
                                 hashs[tagname] = newValueHash
                                 # Publish
-                                mqttClient.publish(
-                                    mqttTopicPublishData, payload=json.dumps(pObject)
-                                )
+                                publish(pObject)
                 time.sleep(sampleTime)
         except Exception as e:
-            print(e)
+            logging.exception("Exception in connection loop.")
         time.sleep(10)
 finally:
     if clientPlc is not None:
